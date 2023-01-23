1018e50d3ffa1196851ea0352d72567a69232589
Haciyev Elnur <hcyevelnur@Haciyevs-MacBook-Air.local>
2023-01-23 14:22:02 +0400

    commit

--- script.js
+++ script.js
@@ -1,96 +1,28 @@
-const display = document.querySelector('.calculator-input');
-const keys = document.querySelector('.calculator-keys');
-
-let silme = '0';
-let birincidəyər = null;
-let operator = null;
-let ikincidəyər = false;
-
-updateDisplay();
-
-function updateDisplay() {
-    display.value = silme;
+function insrt(num) {
+    $('.calculator-input').val($('.calculator-input').val()+num);
 }
 
-keys.addEventListener('click', function(e) {
-    const element = e.target;
-    const value = element.value;
-
-    if (!element.matches('button')) return;
-
-    switch(value) {
-        case '+':
-        case '-':
-        case '*':
-        case '/':
-        case '=':
-            handleOperator(value);
-            break;
-        case '.':
-            inputDecimal();
-            break;
-        case 'clear':
-            clear();
-            break;
-        default:
-            inputNumber(element.value);        
-    }
-    updateDisplay();
-});
-
-function handleOperator(nextOperator) {
-    const value = parseFloat(silme);
-
-    if(operator && ikincidəyər) {
-        operator = nextOperator;
-        return;
-    }
-
-    if (birincidəyər === null) {
-        birincidəyər = value;
-    } else if (operator) {
-        const result = calculate(birincidəyər, value, operator);
-
-        silme = `${parseFloat(result.toFixed(7))}`;
-        birincidəyər = result;
-    }
-
-    ikincidəyər = true;
-    operator = nextOperator;
-
-    console.log(silme, birincidəyər, operator, ikincidəyər);
+function eql(){
+    $('.calculator-input').val(eval($('.calculator-input').val()))
 }
 
-function calculate(first, second, operator) {
-    if(operator === '+') {
-        return first + second;
-    } else if (operator === '-') {
-        return first - second;
-    } else if (operator === '*') {
-        return first * second
-    } else if (operator === '/') {
-        return first / second;
-    }
-    return second;
+function c(){
+    $('.calculator-input').val('');
 }
 
-function inputNumber(num) {
-    if(ikincidəyər) {
-        silme = num;
-        ikincidəyər = false;
-    } else {
-        silme = silme === '0'? num: silme + num;
-    }
+// function del() {
+//     value = $('.calculator-input').val();
+//     $('.calculator-input').val(value.substring(0, value.lenght - 1 ));
+// }
 
-    console.log(silme, birincidəyər, operator, ikincidəyər);
+function del() {
+	value = $('.calculator-input').val();
+	$('.calculator-input').val(value.substring(0, value.length - 1));
+    
 }
 
-function inputDecimal() {
-    if (!silme.includes('.')) {
-        silme += '.';
-    }
-}
 
-function clear() {
-    silme = '0';
-}
+function cle() {
+    document.form.result.value = 0;
+  }
+
